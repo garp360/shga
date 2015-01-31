a13521e13f14ffb632e1f8f5e348e569aa7a8ede
garth <garth@192.168.1.89>
2015-01-31 06:45:13 -0500

    updated hcp entry from text to dropdown

--- shga_WEB/WebContent/js/golfer/controller.profile.js
+++ shga_WEB/WebContent/js/golfer/controller.profile.js
@@ -12,7 +12,13 @@ angular.module('shgaApp.controllers.Profile', []).controller('ProfileController'
 		$log.info('Loaded Profile! ' + userProfile.firstName);
 		$scope.profile = _getProfile(userProfile);
 		$log.info('Loaded Profile! ' + $scope.profile.teebox.color);
-
+		$scope.hcps = [
+           -5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,
+           10,11,12,13,14,15,16,17,18,19,
+           20,21,22,23,24,25,26,27,28,29,30,31,
+           32,33,34,35,36
+		];
+		
 		$scope.teeboxes = [ {
 			color : 'Gold'
 		}, {
@@ -28,13 +34,21 @@ angular.module('shgaApp.controllers.Profile', []).controller('ProfileController'
 		} ];
 
 		var found = false;
-
+		angular.forEach($scope.hcps, function(hcp) {
+			if (hcp == $scope.profile.hcp && !found) {
+				found = true;
+				$scope.profile.hcp = hcp;
+			}
+		});
+		
+		found = false;
 		angular.forEach($scope.teeboxes, function(teebox) {
 			if (teebox.color.toLowerCase() == $scope.profile.teebox.color.toLowerCase() && !found) {
 				found = true;
 				$scope.profile.teebox = teebox;
 			}
 		});
+		
 		$scope.isloaded = true;
 	});
 	
--- shga_WEB/WebContent/js/user/controller.registration.js
+++ shga_WEB/WebContent/js/user/controller.registration.js
@@ -9,8 +9,15 @@ angular.module('shgaApp.controllers.Registration', []).controller('RegistrationC
        { color : 'Green' }, 
        { color : 'Burgundy' }];
 	
+	$scope.hcps = [
+       -5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,
+       10,11,12,13,14,15,16,17,18,19,
+       20,21,22,23,24,25,26,27,28,29,30,31,
+       32,33,34,35,36
+	];
+	
 	$scope.registrant = {
-	    hcp : 10.0,
+	    hcp : $scope.hcps[15],
 	    teebox : $scope.teeboxes[1]
 	};
 	
